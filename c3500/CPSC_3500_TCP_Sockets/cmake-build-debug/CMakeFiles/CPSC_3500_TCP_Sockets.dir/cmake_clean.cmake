21d773fced70ca08635e08760dadb99db1304dac
Lonestar827 <30672524+Lonestar827@users.noreply.github.com>
2023-11-17 12:50:35 -0800

    Add files via upload

---
+++ c3500/CPSC_3500_TCP_Sockets/cmake-build-debug/CMakeFiles/CPSC_3500_TCP_Sockets.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPSC_3500_TCP_Sockets.dir/client-tcp-skeleton.cpp.o"
+  "CMakeFiles/CPSC_3500_TCP_Sockets.dir/server-tcp-skeleton.cpp.o"
+  "CPSC_3500_TCP_Sockets"
+  "CPSC_3500_TCP_Sockets.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPSC_3500_TCP_Sockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
